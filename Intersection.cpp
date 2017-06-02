0d17d39c2d49b9a4548b3d7dc51627a6d8baa353
32674533 <walking1997@live.com.au>
2017-06-02 10:41:40 +0800

    Add files via upload

---
+++ Intersection.cpp
@@ -0,0 +1,18 @@
+#include "Intersection.h"
+
+Intersection::Intersection(bool intersect, float dist){
+	doesIntersect = intersect;
+	distance = dist;
+}
+
+Intersection::~Intersection(void){
+	
+}
+
+bool Intersection::GetDoesInteract(){
+	return doesIntersect;
+}
+
+float Intersection::GetDistance(){
+	return distance;
+}
